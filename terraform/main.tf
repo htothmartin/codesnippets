de8ae56d3a6b74000c5428572a40250a2dd6025f
Martin Hajagos-Toth <htothmartin@gmail.com>
2025-12-09 21:56:01 +0100

    Terraform, ansible, nginx

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  credentials = file("gcp-key.json")
+  project = "code-snippets-480514"
+
+  region = "us-central1"
+  zone = "us-central1-a"
+}
+
+resource "google_compute_instance" "devops_server" {
+  name         = "devops-vm"
+  machine_type = "e2-micro"
+  tags         = ["web-server", "http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 28
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("id_rsa.pub")}"
+  }
+}
+
+resource "google_compute_firewall" "allow_web" {
+  name    = "allow-web-traffic-free"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443", "3000", "5000", "9090", "9100"] 
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web-server"]
+}
+
+output "ip" {
+  value = google_compute_instance.devops_server.network_interface.0.access_config.0.nat_ip
+}
